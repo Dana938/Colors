a3dfaf929027db6eb4b734e1aadd3ce8c7085eff
Jin Jae-yeon <daramkun@live.com>
2019-12-18 00:42:24 +0900

    Add Multitouch Joint color change button feature, UI Scaling for Variable screens

--- Assets/Scripts/In Game/GameStates.cs
+++ Assets/Scripts/In Game/GameStates.cs
@@ -1,49 +1,57 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+public enum GameColor
+{
+    Cyan,
+    Magenta,
+    Yellow,
+    Gray,
+
+    PurpleBlue,     //< Cyan + Magenta
+    LightGreen,     //< Cyan + Yellow
+    EmainMachaBlue, //< Cyan + Gray
+    LightRed,       //< Magenta + Yellow
+    Purple,         //< Magenta + Gray
+    Gold,           //< Yellow + Gray
+}
+
 public class GameStates : MonoBehaviour
 {
+    public static Color GetColor ( int color )
+    {
+        switch (color)
+        {
+            case 0: return new Color ( 0, 1, 1 );                   //< Cyan
+            case 1: return new Color ( 1, 0, 1 );                   //< Magenta
+            case 2: return new Color ( 1, 1, 0 );                   //< Yellow
+            case 3: return new Color ( 0.498f, 0.498f, 0.498f );    //< Gray(Black)
+
+            case 4: return ( GetColor ( 0 ) + GetColor ( 1 ) ) / 2; //< Purple Blue
+            case 5: return ( GetColor ( 0 ) + GetColor ( 2 ) ) / 2; //< Light Green
+            case 6: return ( GetColor ( 0 ) + GetColor ( 3 ) ) / 2; //< Emain-Macha Blue
+            case 7: return ( GetColor ( 1 ) + GetColor ( 2 ) ) / 2; //< Light Red
+            case 8: return ( GetColor ( 1 ) + GetColor ( 3 ) ) / 2; //< Purple
+            case 9: return ( GetColor ( 2 ) + GetColor ( 3 ) ) / 2; //< Gold
+
+            default: throw new ArgumentOutOfRangeException ( "color" );
+        }
+    }
+
     public GameObject GameOverBackground;
 
     public int Color { get; private set; }
     public bool IsGameOver => GameOverBackground.activeSelf;
 
-    public void ChangeColor (int color)
-	{
-        Color = color;
-
+    public void ChangeColor ( int color )
+    {
         Camera camera = GameObject.Find ( "Main Camera" ).GetComponent<Camera> ();
-
-        switch ( color )
-        {
-            // Cyan
-            case 0:
-                camera.backgroundColor = new Color ( 0, 1, 1 );
-                Debug.Log ( "Color To Cyan." );
-                break;
-
-            // Magenta
-            case 1:
-                camera.backgroundColor = new Color ( 1, 0, 1 );
-                Debug.Log ( "Color To Magenta." );
-                break;
-            
-            // Yellow
-            case 2:
-                camera.backgroundColor = new Color ( 1, 1, 0 );
-                Debug.Log ( "Color To Yellow." );
-                break;
-
-            // Black
-            case 3:
-                camera.backgroundColor = new Color ( 0.498f, 0.498f, 0.498f );
-                Debug.Log ( "Color To Black." );
-                break;
-        }
-	}
+        camera.backgroundColor = GetColor ( Color = color );
+    }
 
     // Start is called before the first frame update
     void Start()
--- Assets/Scripts/In Game/UI/GameButton.cs
+++ Assets/Scripts/In Game/UI/GameButton.cs
@@ -8,21 +8,66 @@ public class GameButton : MonoBehaviour
     public void ChangeColorToMagenta () => GetGameStates ().ChangeColor ( 1 );
     public void ChangeColorToYellow () => GetGameStates ().ChangeColor ( 2 );
     public void ChangeColorToBlack () => GetGameStates ().ChangeColor ( 3 );
+    public void ChangeColorToPurpleBlue () => GetGameStates ().ChangeColor ( 4 );
+    public void ChangeColorToLightGreen () => GetGameStates ().ChangeColor ( 5 );
+    public void ChangeColorToEmainMachaBlue () => GetGameStates ().ChangeColor ( 6 );
+    public void ChangeColorToLightRed () => GetGameStates ().ChangeColor ( 7 );
+    public void ChangeColorToPurple () => GetGameStates ().ChangeColor ( 8 );
+    public void ChangeColorToGold () => GetGameStates ().ChangeColor ( 9 );
 
     GameStates GetGameStates ()
     {
         return GameObject.Find ( "Main Camera" ).GetComponent<GameStates> ();
     }
 
-    // Start is called before the first frame update
-    void Start ()
-    {
+    List<GameColor> touchedColors = new List<GameColor> ( 2 );
 
+    public void CyanDown () => ButtonDown ( GameColor.Cyan );
+    public void MagentaDown () => ButtonDown ( GameColor.Magenta );
+    public void YellowDown () => ButtonDown ( GameColor.Yellow );
+    public void BlackDown () => ButtonDown ( GameColor.Gray );
+    private void ButtonDown ( GameColor color )
+    {
+        if ( touchedColors.Count < 2 )
+        {
+            touchedColors.Add ( color );
+
+            if ( touchedColors.Count == 1 )
+            {
+                switch ( color )
+                {
+                    case GameColor.Cyan: ChangeColorToCyan (); break;
+                    case GameColor.Magenta: ChangeColorToMagenta (); break;
+                    case GameColor.Yellow: ChangeColorToYellow (); break;
+                    case GameColor.Gray: ChangeColorToBlack (); break;
+                }
+            }
+            else
+            {
+                bool cyanIn = touchedColors.Contains ( GameColor.Cyan ),
+                    magentaIn = touchedColors.Contains ( GameColor.Magenta ),
+                    yellowIn = touchedColors.Contains ( GameColor.Yellow ),
+                    blackIn = touchedColors.Contains ( GameColor.Gray );
+
+                if ( cyanIn && magentaIn ) ChangeColorToPurpleBlue ();
+                else if ( cyanIn && yellowIn ) ChangeColorToLightGreen ();
+                else if ( cyanIn && blackIn ) ChangeColorToEmainMachaBlue ();
+                else if ( magentaIn && yellowIn ) ChangeColorToLightRed ();
+                else if ( magentaIn && blackIn ) ChangeColorToPurple ();
+                else if ( yellowIn && blackIn ) ChangeColorToGold ();
+            }
+        }
     }
 
-    // Update is called once per frame
-    void Update ()
+    public void CyanUp () => ButtonUp ( GameColor.Cyan );
+    public void MagentaUp () => ButtonUp ( GameColor.Magenta );
+    public void YellowUp () => ButtonUp ( GameColor.Yellow );
+    public void BlackUp () => ButtonUp ( GameColor.Gray );
+    private void ButtonUp ( GameColor color )
     {
-
+        if ( touchedColors.Contains ( color ) )
+        {
+            touchedColors.Remove ( color );
+        }
     }
 }
